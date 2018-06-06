5cf65ec2ed97e6a528603a519d9a79e5c9809c5e
Atanas Dragolov <adragolov@gmail.com>
2018-06-06 15:04:23 +0300

    Adding stub for prime numbers generator algorithm.

---
+++ lib/FCPrimesMultiplicationTableLib/MathServices/primeNumFinder.rb
@@ -0,0 +1,18 @@
+module FCPrimesMultiplicationTableLib
+    
+    module MathServices
+        
+        class PrimeNumFinder
+
+            public 
+            
+            def getPrimesUntil(n)
+
+            end
+
+            def getFirstNPrimes(n)
+                
+            end
+        end
+    end
+end
